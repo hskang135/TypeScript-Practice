8cbb4766a4cc3542468e40e5de84b8868d56e9ee
Heesu Kang <hskang135@gmail.com>
2019-12-23 16:48:21 -0500

    still

--- hello.js
+++ hello.js
@@ -1,12 +1,10 @@
 var message = 'Hello World';
 console.log(message);
-
 var isDone = false;
 var decimal = 10;
 var hex = 0xf00d;
 var binary = 10;
 var octal = 484;
-
 var color = "red";
 color = 'white';
 var apple = [1, 2, 3];
@@ -15,19 +13,30 @@ var x;
 x = ["hello", 100];
 // x = [200, "hello"]; // error
 console.log(x[0].substring(1));
-
 // enum Color {Red=1, Blue, Green}
 // let c: Color = Color.Green;
-
 var Color;
 (function (Color) {
-  Color[Color["Red"] = 1] = "Red";
-  Color[Color["Green"] = 2] = "Green";
-  Color[Color["Blue"] = 3] = "Blue";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Green"] = 2] = "Green";
+    Color[Color["Blue"] = 3] = "Blue";
 })(Color || (Color = {}));
+;
 var colorName = color[2];
 console.log(colorName);
-
 var notSure = 4;
 notSure = "maybe";
 notSure = false;
+// let notSures: any = 4;
+// notSures.ifItExists();
+// notSures.toFixed();
+var lists = [1, true, "free"];
+lists[1] = 100;
+var name = "John";
+var score1 = 50;
+var score2 = 42.50;
+var sum = score1 + score2;
+console.log("name" + name);
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of the scores: " + sum);
--- hello.ts
+++ hello.ts
@@ -21,11 +21,28 @@ console.log(x[0].substring(1));
 // enum Color {Red=1, Blue, Green}
 // let c: Color = Color.Green;
 
-enum Color {Red = 1, Green, Blue}
+enum Color {Red = 1, Green, Blue};
 let colorName: string = color[2];
 console.log(colorName);
 
 let notSure: any = 4;
 notSure = "maybe";
 notSure = false;
+
+// let notSures: any = 4;
+// notSures.ifItExists();
+// notSures.toFixed();
+
+let lists: any[] = [1, true, "free"];
+lists[1] = 100;
+
+var name:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50;
+var sum = score1 + score2;
+
+console.log("name"+name); 
+console.log("first score: "+score1);
+console.log("second score: "+score2); 
+console.log("sum of the scores: "+sum);
 
